d14252ce2f46fb3f8efe29dffbffd4ef39d643e3
Quit <quitcrypto@gmail.com>
2024-11-04 16:06:18 -0800

    Rename HotWalletProxy -> Warm

--- script/Warm.s.sol
+++ script/Warm.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Warm} from "../src/Warm.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract WarmScript is Script {
+    Warm public warm;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        warm = new Warm();
 
         vm.stopBroadcast();
     }
