675fc0d6e1c36f8a8f68e9cbe42b981da37011ee
Coffee☕️ <coffee@coffeebot.dev>
2025-01-17 12:26:27 -0500

    zk deploy

--- script/DeployZkEVM.s.sol
+++ script/DeployZkEVM.s.sol
@@ -19,9 +19,12 @@ interface ImmutableCreate2Factory {
 contract DeployZkEVM is Script {
     ExclusiveDelegateResolver public resolver;
 
+    address delegateRegistryAddress = 0x0000000059A24EB229eED07Ac44229DB56C5d797;
+    bytes32 salt = 0x000000000000000000000000000000000000000052a2c3b1b2e2b6d8b7c40016; // 0x0000000078CC4Cc1C14E27c0fa35ED6E5E58825D
+
     function setUp() public {}
 
-    function run(address delegateRegistryAddress, bytes32 salt) public {
+    function run() public {
         vm.startBroadcast();
 
         // deploy resolver
