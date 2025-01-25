2ec463faff287a5739f7f0467eb187b1bb704390
Coffee☕️ <coffee@coffeebot.dev>
2025-01-24 21:05:01 -0500

    fix: use floating pragma for interface for maximum compatibility

--- src/IExclusiveDelegateResolver.sol
+++ src/IExclusiveDelegateResolver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.0;
 
 import {IDelegateRegistry} from "./interfaces/IDelegateRegistry.sol";
 
@@ -10,11 +10,23 @@ interface IExclusiveDelegateResolver {
 
     function exclusiveWalletByRights(address vault, bytes24 rights) external view returns (address wallet);
 
-    function delegatedWalletsByRights(address wallet, bytes24 rights) external view returns (address[] memory wallets);
+    function delegatedWalletsByRights(address wallet, bytes24 rights)
+        external
+        view
+        returns (address[] memory wallets);
 
-    function exclusiveOwnerByRights(address contractAddress, uint256 tokenId, bytes24 rights) external view returns (address owner);
+    function exclusiveOwnerByRights(address contractAddress, uint256 tokenId, bytes24 rights)
+        external
+        view
+        returns (address owner);
 
-    function decodeRightsExpiration(bytes32 rights) external pure returns (bytes24 rightsIdentifier, uint40 expiration);
+    function decodeRightsExpiration(bytes32 rights)
+        external
+        pure
+        returns (bytes24 rightsIdentifier, uint40 expiration);
 
-    function generateRightsWithExpiration(bytes24 rightsIdentifier, uint40 expiration) external pure returns (bytes32);
+    function generateRightsWithExpiration(bytes24 rightsIdentifier, uint40 expiration)
+        external
+        pure
+        returns (bytes32);
 }
